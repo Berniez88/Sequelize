2783d5bfebec637937134a94521ee4c01732a617
bzhong88 <berniezhong@gmail.com>
2021-03-29 14:33:54 -0400

    updated lab

---
+++ public/script.js
@@ -0,0 +1,50 @@
+/*import DiningHall from '../models/DiningHall';*/
+
+/*async function getData() {
+  const response = await fetch('/api/dining');
+  const alpha = await response.json();
+  const dining = alpha.data;
+  return dining;
+}
+
+
+function diningHall() {
+  const bravo = document.querySelector('.target');
+  dining.forEach((element) => {
+    const charlie = document.createElement("tr");
+    charlie.innerHTML = `
+    <td>${element.hall_id}</td>
+    <td>${element.hall_name}</td>
+    <td>${element.hall_address} </td>`;
+    
+    bravo.append(charlie)
+  });
+
+}
+
+async function windowActions(){
+  const alpha = diningHall();
+  await getData(alpha)
+}
+
+window.onload = windowActions(); */
+
+
+async function dinner () {
+    const dinnerFetch = await fetch('/api/dining');
+    const dinnerInfo = await dinnerFetch.json();
+    const arrayDinner = dinnerInfo.data; 
+    const information = document.querySelector('.target');
+  
+    arrayDinner.forEach((x) => {
+        const makeRows = document.createElement('tr');
+        makeRows.innerHTML = `
+                <td>${x.hall_id}</td>
+                <td>${x.hall_name}</td>
+                <td>${x.hall_address}</td>   
+        `;
+        information.append(makeRows)
+    });
+  }
+  
+  window.onload = dinner();
--- server.js
+++ server.js
@@ -9,11 +9,10 @@ const PORT = process.env.PORT || 3000;
 const staticFolder = "public";
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static(staticFolder))
 app.use('/api', apiRoutes);
 
 
-app.use(express.static(staticFolder))
 
 async function bootServer() {
   try {
